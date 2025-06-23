a1a42111e0035509195a65777503e22cb8833d3b
munir sebikaaka <munirsebikaaka@gmail.com>
2025-06-23 20:24:43 +0300

    testing deployment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import "../styles/home.css";
 function Home({ user }) {
   return (
     <div className="home">
-      <h1>Welcome to the Marketplace</h1>
+      {/* <h1>Welcome to the Marketplace</h1> */}
       <p>Buy and sell products with ease</p>
 
       {!user && (
